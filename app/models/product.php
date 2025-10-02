5c3a9dc55c1120cf55de0ab4acf91fc1fd2ae6cc
Natalia Sergeeva <natasg777@yandex.ru>
2025-10-02 20:53:17 +0300

    add product search on index page

--- app/models/product.php
+++ app/models/product.php
@@ -32,12 +32,84 @@ class Product{
         $stmt->execute();
     }
 
-    public static function getProducts() {
+    public static function getQueryProducts($name=null) {
         $instance = new self();
-        $stmt = $instance->db->prepare('SELECT * FROM products JOIN images ON images.product_id = products.id');                          
+        $sql = 'SELECT  products.id as product_id,
+                        products.name,
+                        products.price, 
+                        images.id as image_id,
+                        images.filepath
+                FROM products JOIN images ON images.product_id = products.id ';
+        if ($name!=null) {
+            $sql .= 'WHERE name REGEXP :name';
+        } else {
+            $sql .= 'ORDER BY RAND()';
+        };                                
+        $stmt= $instance->db->prepare($sql);   
+        if ($name!=null) {
+            $stmt->bindParam(':name', $name);                    
+        }  
         $stmt->execute();
-        return $stmt->fetchAll();
-    }
-        
+        $products = $stmt->fetchAll();
 
+        // getting array of colors
+        $productIds = array_column($products, 'product_id');
+        $placeholders = str_repeat('?,', count($productIds) - 1) . '?';
+        $sql = "SELECT DISTINCT colors.color, colors.id 
+                FROM products 
+                JOIN colors ON colors.id = products.color_id 
+                WHERE products.id IN ($placeholders)";
+        $stmt = $instance->db->prepare($sql);
+        $stmt->execute($productIds);
+        $colors = $stmt->fetchAll();
+
+        // getting array of categories
+        $sql = "SELECT DISTINCT categories.name , categories.id
+                FROM product_category
+                JOIN categories ON categories.id = product_category.category_id 
+                WHERE product_category.product_id IN ($placeholders)";
+        $stmt = $instance->db->prepare($sql);
+        $stmt->execute($productIds);
+        $categories = $stmt->fetchAll();
+
+        $_SESSION['query_products_id'] = $productIds;
+        $_SESSION['query_products_colors'] = $colors;
+        $_SESSION['query_products_categories'] = $categories;
+
+        return [
+            'products' => $products,
+            'colors' => $colors,
+            'categories' => $categories
+        ];
+    }    
+
+    public static function getFilters ($colorIds, $categoriesIds, $maxCost) {
+        $product_IDs = $_SESSION['query_products_id'];
+
+        $placeholders_prod = str_repeat('?,', count($product_IDs) - 1) . '?';
+        $placeholders_colors = str_repeat('?,', count($colorIds) - 1) . '?';
+        $placeholders_categories = str_repeat('?,', count($categoriesIds) - 1) . '?';
+        $params = array_merge($product_IDs, $colorIds,$categoriesIds);
+
+        $instance = new self(); 
+        $sql = "SELECT  products.id as product_id,
+                        products.name,
+                        products.price, 
+                        images.id as image_id,
+                        images.filepath
+                FROM products 
+                JOIN images ON images.product_id = products.id
+                JOIN product_category ON product_category.product_id = products.id
+                WHERE products.id IN ($placeholders_prod) 
+                AND products.color_id IN ($placeholders_colors)
+                AND product_category.category_id IN ($placeholders_categories)";
+        $stmt = $instance->db->prepare($sql);
+        $stmt->execute($params);
+        $products = $stmt->fetchAll();
+        return [
+            'products' => $products,
+            'colors' => $_SESSION['query_products_colors'],
+            'categories' => $_SESSION['query_products_categories']
+        ];        
+    }
 }
--- templates/index.php
+++ templates/index.php
@@ -2,17 +2,52 @@
 
 includeTemplate('header.php');
 
-$products = Product::getProducts();
 
+if (isset($_POST['submit_query'])) {
+    $querytext=$_POST['name'] ?? '';
+    $products = Product::getQueryProducts($querytext);
+} else {
+    $products = Product::getQueryProducts(null);
+}
+
+
+if (isset($_POST['extended_search'])) {
+    $queryColors=$_POST['colors'] ?? '';
+    $queryCategories=$_POST['categories'] ?? '';
+    $querySlider=$_POST['price_range'] ?? '';
+    $products = Product::getFilters($queryColors, $queryCategories, $querySlider);
+}
 ?>
 
     <form class="form" method="post" >
         <input type="text" name="name">
-        <button  type="submit" name="submitQuery"><img src="/public/css/magnifying-glass-silhouette.png">  </button>
+        <button  type="submit" name="submit_query"><img src="/public/css/magnifying-glass-silhouette.png">  </button>
     </form>
 
+<div class="products_window">
+    <aside>
+    <h2>Расширенный поиск </h2>
+        <form id="form_extended_search" method="POST">
+            <label class="label_search">Цена</label>
+                <input type="text" name="minPrice">
+                <input type="text" name="maxPrice">
+            <label class="label_search">Цвет</label><br>
+                <?php foreach($products['colors'] as $color) {?>
+                    <input name="colors[]"  value="<?= $color['id'] ?>" type="checkbox" checked>
+                    <label > <?= $color['color'] ?> </label><br>
+                <?php }?>
+            <label for="">Категория товара</label><br>        
+                <?php foreach($products['categories'] as $category) {?>
+                    <input name="categories[]" value="<?= $category['id'] ?>"  type="checkbox" checked>
+                    <label > <?= $category['name'] ?> </label><br>
+                <?php }?>
+            <button type="submit" name="extended_search">Применить</button>
+        </form>
+    </aside>
+    <?php   includeTemplate('products/product.php', ['products' => $products['products']]); ?>
+</div>
 
-<?php   includeTemplate('products/product.php', ['products' => $products]); ?>
+
 
 <?php   includeTemplate('footer.php');  ?>
 
